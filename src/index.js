eeebf0003f8df0385b1e980f8899125d99b4ec63
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-11-26 07:39:51 -0500

    Intro to contact cards
    Started creating the contact cards

--- src/index.js
+++ src/index.js
@@ -5,10 +5,28 @@ import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Responsive Contact Info</h1>
-      <div>Blach</div>
-    </div>
+    <body>
+      <div className="contact-info">
+        <div className="card">
+          <i className="card-icon far fa-envelope" />
+          <p>l.hutchinsonj@gmail.com</p>
+        </div>
+      </div>
+
+      <div className="contact-info">
+        <div className="card">
+          <i className="card-icon fas fa-phone" />
+          <p>0000000000</p>
+        </div>
+      </div>
+
+      <div className="contact-info">
+        <div className="card">
+          <i className="card-icon fas fa-map-marker-alt" />
+          <p>New York</p>
+        </div>
+      </div>
+    </body>
   );
 }
 
