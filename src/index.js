9c7fda72c1586281b6d4f5e6e681ed49b3f23664
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-11-26 10:48:37 -0500

    Completed Cards
    Completed Address cards

--- src/index.js
+++ src/index.js
@@ -6,24 +6,20 @@ import "./styles.css";
 function App() {
   return (
     <body>
-      <div className="contact-info">
-        <div className="card">
-          <i className="card-icon far fa-envelope" />
-          <p>l.hutchinsonj@gmail.com</p>
+      <div class="contact-info">
+        <div class="card">
+          <i class="card-icon far fa-envelope" />
+          <p>email@domain.com</p>
         </div>
-      </div>
 
-      <div className="contact-info">
-        <div className="card">
-          <i className="card-icon fas fa-phone" />
-          <p>0000000000</p>
+        <div class="card">
+          <i class="card-icon fas fa-phone" />
+          <p>+000000000000</p>
         </div>
-      </div>
 
-      <div className="contact-info">
-        <div className="card">
-          <i className="card-icon fas fa-map-marker-alt" />
-          <p>New York</p>
+        <div class="card">
+          <i class="card-icon fas fa-map-marker-alt" />
+          <p>New York, USA</p>
         </div>
       </div>
     </body>
